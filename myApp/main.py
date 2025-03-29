7d652bd2d8dbc3d00953d498aae803df823669f9
Andrii K <andriykorotchuk345@gmail.com>
2025-03-29 19:32:21 +0200

    Add calculator

---
+++ myApp/main.py
@@ -0,0 +1,34 @@
+def add(a, b):
+    return a + b
+
+def substraction(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if(b == 0):
+        return ZeroDivisionError 
+    
+    else:
+        return a / b
+
+def power(a, b):
+    return a ** b
+
+def equal(a, b):
+    if(a == b):
+        return "Equal"
+    
+    else:
+        return "Not equal"
+    
+print(add(2, 5))
+print(substraction(7, 2))
+print(multiply(3, 3))
+print(divide(50, 25))
+print(power(3, 3))
+print(equal(3, 1))
+
+
---
+++ tests/test_main.py
@@ -0,0 +1,22 @@
+from myApp.main import add, substraction, multiply, divide, power, equal
+
+def test_add():
+    assert add(11, 3) == 14
+
+def test_substraction():
+    assert substraction() == 10
+
+def test_multiply():
+    assert multiply(5, 5) == 25
+
+def test_divide():
+    assert divide(3, 0) == ZeroDivisionError
+    assert divide(35, 7) == 5
+
+def test_power():
+    assert power(20, 0) == 1
+    assert power(3, 3) == 9
+
+def equal():
+    assert equal(2, 5) == "Not equal"
+    assert equal(1, 1) == "Equal"
