232703fe5d872fe714ba3989c4cfb4d466693fc8
Andrii K <andriykorotchuk345@gmail.com>
2025-03-29 19:51:45 +0200

    Comented functional for Task 1

--- myApp/main.py
+++ myApp/main.py
@@ -1,38 +1,40 @@
 def hello_to_github():
     return print("Hello GitHub Actions!")
 
-def add(a, b):
-    return a + b
+# def add(a, b):
+#     return a + b
 
-def substraction(a, b):
-    return a - b
+# def substraction(a, b):
+#     return a - b
 
-def multiply(a, b):
-    return a * b
+# def multiply(a, b):
+#     return a * b
 
-def divide(a, b):
-    if(b == 0):
-        return ZeroDivisionError 
+# def divide(a, b):
+#     if(b == 0):
+#         return ZeroDivisionError 
     
-    else:
-        return a / b
+#     else:
+#         return a / b
 
-def power(a, b):
-    return a ** b
+# def power(a, b):
+#     return a ** b
 
-def equal(a, b):
-    if(a == b):
-        return "Equal"
+# def equal(a, b):
+#     if(a == b):
+#         return "Equal"
     
-    else:
-        return "Not equal"
+#     else:
+#         return "Not equal"
 
 hello_to_github()
-print(add(2, 5))
-print(substraction(7, 2))
-print(multiply(3, 3))
-print(divide(50, 25))
-print(power(3, 3))
-print(equal(3, 1))
+# print(add(2, 5))
+# print(substraction(7, 2))
+# print(multiply(3, 3))
+# print(divide(50, 25))
+# print(power(3, 3))
+# print(equal(3, 1))
+
+
 
 
